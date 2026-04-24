6ea6cb97177e1fd75940ce6646d821b99c9562ae
Dev-Rayyan Ahmed <rayyanahmed1624@gmail.com>
2026-04-25 02:02:55 +0500

    feat: added user.controller (create/add)

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,119 +1,173 @@
 import { NextFunction, Request, Response } from "express";
-import ApiError from "../utils/ApiError.js";
+import bcrypt from "bcryptjs";
 import pool from "../db/index.js";
+import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 
+const SALT_ROUNDS = 12;
+
+// PostgreSQL Error Code;
+// 23503 : Foreign Key Voilation
+// 23505 : Unique Value Voilation
+// 23514 : Not Null Voilation
+
 export async function createAdmin(req: Request, res: Response, next: NextFunction) {
-  if (!req.body) {
-    return next(new ApiError(422, "Unprocessable Entity", "Body is missing"))
+
+    // checking obj keys in case of empty {}
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return next(new ApiError(422, "Unprocessable Entity", "Body is missing"));
   }
 
-  const { userName, email, password, departmentId, adminLevel } = req.body;
+  const { userId, userName, userEmail, userContactNo, password, deptAbbreviation, adminLvl } = req.body;
 
-  if ([userName, email, password].some((field) => !field)) {
-    return next(new ApiError(422, "Unpocessable Entity", "All fields are required"));
+  if ([userId, userName, userEmail, password, deptAbbreviation, adminLvl].some((field)=>!field)) {
+    return next(new ApiError(422, "Unprocessable Entity", "All required fields must be provided"));
   }
 
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
   const client = await pool.connect();
+
   try {
-    const emailExists = await client.query(`Select 1 from Users Where email = $1`, [email]);
-    if (emailExists.rowCount) {
-      return next(new ApiError(409, "Conflict", "Email already exits"));
-    }
-
-    const userNameExists = await client.query(`Select 1 from Users Where user_name = $1`, [
-      userName
-    ]);
-    if (userNameExists.rowCount) {
-      return next(new ApiError(409, "Conflict", "userNamealready exits"));
-    }
-
-    // currenlty not hashing the password but  TODO:
     await client.query("BEGIN");
 
     const userQuery = `
-            INSERT INTO users (user_name, email, password, user_type)
-            VALUES ($1, $2, $3, 'admin') RETURNING id, user_name AS "userName", email, user_type AS "userType"`;
-    const userRes = await client.query(userQuery, [userName, email, password]);
-    const newUserId = userRes.rows[0].id;
+      INSERT INTO users (user_id, user_name, user_email, user_contact_no, password, dept_abbreviation)
+      VALUES ($1, $2, $3, $4, $5, $6) 
+      RETURNING user_id AS "userId", user_name AS "userName", user_email AS "userEmail", dept_abbreviation AS "deptAbbreviation"
+    `;
+    const userRes = await client.query(userQuery, [userId, userName, userEmail, userContactNo, hashedPassword, deptAbbreviation]);
 
     const adminQuery = `
-            INSERT INTO admins (id, department_id, admin_level) 
-            VALUES ($1, $2, $3) RETURNING department_id AS "departmentId", admin_level as "adminLevel"`;
-    const adminRes = await client.query(adminQuery, [newUserId, departmentId, adminLevel]);
+      INSERT INTO admin (user_id, admin_lvl) 
+      VALUES ($1, $2) 
+      RETURNING admin_lvl AS "adminLvl"
+    `;
+    const adminRes = await client.query(adminQuery, [userId, adminLvl]);
 
     const adminData = { ...userRes.rows[0], ...adminRes.rows[0] };
-    client.query("COMMIT");
+    await client.query("COMMIT");
 
     return res.status(201).json(new ApiResponse(201, adminData, "Admin created successfully"));
-  } catch (error) {
+  } catch (error: any) {
+
     await client.query("ROLLBACK");
+
+    if (error.code === '23505') {
+      return next(new ApiError(409, "Conflict", "User ID or Email already exists"));
+    }
+    if (error.code === '23503') {
+      return next(new ApiError(409, "Conflict", "The specified department abbreviation does not exist"));
+    }
+
     console.error("Transaction Error", error);
-    return next(new ApiError(500, "DATABASE FAILED", "faild to run query"));
+    return next(new ApiError(500, "Internal Server Error", "Failed to create Admin"));
+    
   } finally {
     client.release();
   }
 }
 
 export async function addFaculty(req: Request, res: Response, next: NextFunction) {
-  const { userName, email, password, employeeId, designation, departmentId } = req.body;
-  if ([userName, email, password].some((field) => !field)) {
-    return next(new ApiError(422, "Unpocessable Entity", "All fields are required"));
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return next(new ApiError(422, "Unprocessable Entity", "Body is missing"));
   }
 
+  const { userId, userName, userEmail, userContactNo, password, deptAbbreviation, designation, areaOfResearch } = req.body;
+
+  if ([userId ,userName ,userEmail ,password ,deptAbbreviation, designation].some((field)=> !field)) {
+    return next(new ApiError(422, "Unprocessable Entity", "All required fields must be provided"));
+  }
+
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
   const client = await pool.connect();
 
   try {
-    const emailExists = await client.query(`Select 1 from Users Where email = $1`, [email]);
-    if (emailExists.rowCount) {
-      return next(new ApiError(409, "Conflict", "Email already exits"));
-    }
-
-    const userNameExists = await client.query(`Select 1 from Users Where user_name = $1`, [
-      userName
-    ]);
-    if (userNameExists.rowCount) {
-      return next(new ApiError(409, "Conflict", "userNamealready exits"));
-    }
-
-    // TODO: Hashed the password here or built a utils and call it here;
-
     await client.query("BEGIN");
 
     const userQuery = `
-            INSERT INTO users (user_name, email, password, user_type)
-            VALUES ($1, $2, $3, 'faculty') RETURNING id, user_name,email,user_type`;
-
-    const userRes = await client.query(userQuery, [userName, email, password]);
-    const newUserId = userRes.rows[0].id;
+      INSERT INTO users (user_id, user_name, user_email, user_contact_no, password, dept_abbreviation)
+      VALUES ($1, $2, $3, $4, $5, $6) 
+      RETURNING user_id AS "userId", user_name AS "userName", user_email AS "userEmail"
+    `;
+    const userRes = await client.query(userQuery, [userId, userName, userEmail, userContactNo, hashedPassword, deptAbbreviation]);
 
     const facultyQuery = `
-            INSERT INTO faculty (id, employee_id, designation, department_id)
-            VALUES ($1, $2, $3, $4) RETURNING *`;
+      INSERT INTO faculty (user_id, designation, area_of_research) 
+      VALUES ($1, $2, $3) 
+      RETURNING designation, area_of_research AS "areaOfResearch"
+    `;
+    const facultyRes = await client.query(facultyQuery, [userId, designation, areaOfResearch]);
 
-    const facultyRes = await client.query(facultyQuery, [
-      newUserId,
-      employeeId,
-      designation,
-      departmentId
-    ]);
     const facultyData = { ...userRes.rows[0], ...facultyRes.rows[0] };
     await client.query("COMMIT");
 
-    return res.status(201).json(new ApiResponse(201, facultyData, "Faculty created successfully"));
-  } catch (error) {
+    return res.status(201).json(new ApiResponse(201, facultyData, "Faculty added successfully"));
+
+  } catch (error: any) {
     await client.query("ROLLBACK");
+    if (error.code === '23505') {
+      return next(new ApiError(409, "Conflict", "User ID or Email already exists"));
+    }
+    if (error.code === '23503') {
+      return next(new ApiError(409, "Conflict", "The specified department abbreviation does not exist"));
+    }
+
     console.error("Transaction Error:", error);
     return next(new ApiError(500, "Internal Server Error", "Database transaction failed"));
+
   } finally {
     client.release();
   }
 }
 
 export async function addStaff(req: Request, res: Response, next: NextFunction) {
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return next(new ApiError(422, "Unprocessable Entity", "Body is missing"));
+  }
 
+  const { userId, userName, userEmail, userContactNo, password, deptAbbreviation, jobTitle } = req.body;
 
+  if ([userId, userName, userEmail, password, deptAbbreviation, jobTitle].some((field)=> !field)) {
+    return next(new ApiError(422, "Unprocessable Entity", "All required fields must be provided"));
+  }
+
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+  const client = await pool.connect();
+
+  try {
+    await client.query("BEGIN");
+
+    const userQuery = `
+      INSERT INTO users (user_id, user_name, user_email, user_contact_no, password, dept_abbreviation)
+      VALUES ($1, $2, $3, $4, $5, $6) 
+      RETURNING user_id AS "userId", user_name AS "userName", user_email AS "userEmail"
+    `;
+    const userRes = await client.query(userQuery, [userId, userName, userEmail, userContactNo, hashedPassword, deptAbbreviation]);
+
+    const staffQuery = `
+      INSERT INTO staff (user_id, job_title) 
+      VALUES ($1, $2) 
+      RETURNING job_title AS "jobTitle"
+    `;
+    const staffRes = await client.query(staffQuery, [userId, jobTitle]);
+
+    const staffData = { ...userRes.rows[0], ...staffRes.rows[0] };
+    await client.query("COMMIT");
+
+    return res.status(201).json(new ApiResponse(201, staffData, "Staff added successfully"));
+  } catch (error: any) {
+    await client.query("ROLLBACK");
+    if (error.code === '23505') {
+      return next(new ApiError(409, "Conflict", "User ID or Email already exists"));
+    }
+    if (error.code === '23503') {
+      return next(new ApiError(409, "Conflict", "The specified department abbreviation does not exist"));
+    }
+
+    console.error("Transaction Error:", error);
+    return next(new ApiError(500, "Internal Server Error", "Database transaction failed"));
+    
+  } finally {
+    client.release();
+  }
 }
-
-// TODO: there are some error to resolve 1st of all in api error message is not showing in reposnse 
-// TODO: if body is required and clinet is not sending we want show error in response currently it is doing this only for createAdmin  
