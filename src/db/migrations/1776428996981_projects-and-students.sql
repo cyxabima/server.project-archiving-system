874ea7690763a8bec42ad5fd3f8cfb731ed0c5b9
Dev-Rayyan Ahmed <rayyanahmed1624@gmail.com>
2026-05-16 12:30:31 +0500

    removed: projId from students

--- src/db/migrations/1776428996981_projects-and-students.sql
+++ src/db/migrations/1776428996981_projects-and-students.sql
@@ -12,10 +12,7 @@ CREATE TABLE students (
     seat_no VARCHAR(20) PRIMARY KEY,
     std_name VARCHAR(100) NOT NULL,
     std_email VARCHAR(100) UNIQUE NOT NULL,
-    batch VARCHAR(10) NOT NULL,
-    project_id VARCHAR(20),
-    CONSTRAINT fk_std_proj FOREIGN KEY (project_id) 
-    REFERENCES projects(project_id) ON UPDATE CASCADE ON DELETE SET NULL
+    batch VARCHAR(10) NOT NULL
 );
 
 -- Down Migration
