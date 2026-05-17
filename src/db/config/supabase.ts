0133d551a2f38e4b707b2d59ad1eea9fb53f6186
Dev-Rayyan Ahmed <rayyanahmed1624@gmail.com>
2026-05-17 18:37:39 +0500

    added: create Project endpoint

--- src/db/config/supabase.ts
+++ src/db/config/supabase.ts
@@ -6,7 +6,6 @@ dotenv.config();
 const supabaseUrl = process.env.SUPABASE_URL;
 // CRITICAL: Use the SERVICE_ROLE key for your backend, not the anon key!
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
-
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase environment variables");
 }
