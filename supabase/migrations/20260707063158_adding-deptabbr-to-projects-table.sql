4146dab3b70a0bfa6c2bb479143032c1dbd41e42
Abdul Mueed <mueed9972@gmail.com>
2026-07-08 17:14:26 +0500

    updated stats endpoint

---
+++ supabase/migrations/20260707063158_adding-deptabbr-to-projects-table.sql
@@ -0,0 +1,4 @@
+ALTER TABLE projects
+ADD COLUMN dept_abbreviation VARCHAR(20),
+ADD CONSTRAINT fk_proj_dept FOREIGN KEY (dept_abbreviation)
+    REFERENCES department(dept_abbreviation) ON UPDATE CASCADE ON DELETE SET NULL;
