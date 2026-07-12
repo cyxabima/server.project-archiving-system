627dc31e16073858865c8558644b2123c6a7dbe1
Abdul Mueed <mueed9972@gmail.com>
2026-07-12 18:15:31 +0500

    added logout endpoint

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -75,3 +75,13 @@ export async function login(req: Request, res: Response, next: NextFunction) {
     client.release();
   }
 }
+
+export function logout(req: Request, res: Response) {
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+  });
+
+  return res.status(200).json(new ApiResponse(200, null, "Logged out successfully"));
+}
